6de1e69c4f83136154d22105c14addc0733be28c
reload-kurt <kurt@reload.co.za>
2020-07-06 10:57:55 +0200

    first commit

---
+++ precache-manifest.10d7c88830a50134fef66786a04917de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a268564ed58f036c64b53a6a20927d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c092756843849eb0936",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2271704656b7b0c2f2a8",
+    "url": "/static/css/main.4cde3c29.chunk.css"
+  },
+  {
+    "revision": "6c092756843849eb0936",
+    "url": "/static/js/2.8c059b6e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8c059b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2271704656b7b0c2f2a8",
+    "url": "/static/js/main.c9458c36.chunk.js"
+  },
+  {
+    "revision": "8e8fb987744850106d1c",
+    "url": "/static/js/runtime-main.9e170524.js"
+  }
+]);
