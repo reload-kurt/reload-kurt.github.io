0300f9747439ca3e2e1fef1a09257d08da9b1dff
reload-kurt <kurt@reload.co.za>
2020-07-23 10:28:48 +0200

    updates to winner selection

---
+++ precache-manifest.364319ec0d168ccb4ec862655508f534.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37473f43402e19ae26dbbe6647b5081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61c64d1d638a28d0c616",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6e2710fac19eb914ec8d",
+    "url": "/static/css/main.4cde3c29.chunk.css"
+  },
+  {
+    "revision": "61c64d1d638a28d0c616",
+    "url": "/static/js/2.b83a8696.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b83a8696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2710fac19eb914ec8d",
+    "url": "/static/js/main.e5b85a1a.chunk.js"
+  },
+  {
+    "revision": "8e8fb987744850106d1c",
+    "url": "/static/js/runtime-main.9e170524.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66ad5b9926f95815b4f57b0041806586.js"
+  "/precache-manifest.364319ec0d168ccb4ec862655508f534.js"
 );
 
 self.addEventListener('message', (event) => {
